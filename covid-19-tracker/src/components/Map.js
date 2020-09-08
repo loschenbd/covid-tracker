eac2daaaf51bb8ff303fc8317f956694648d726f
Benjamin Loschen <loschenbd@gmail.com>
2020-09-08 18:30:08 -0500

    Added gh-pages. Minor fixes.

--- covid-19-tracker/src/components/Map.js
+++ covid-19-tracker/src/components/Map.js
@@ -7,8 +7,8 @@ function Map({center, zoom}) {
       <div className="map">
         <LeafletMap center={center} zoom={zoom}>
           <TileLayer
-          url="https://{s}.tile.openstreetmap.org{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright>OpenStreet</a> contributors'
+          url='https://{s}.tile.openstreetmap.org{z}/{x}/{y}.png'
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreet</a> contributors'
           />
         </LeafletMap>
       </div>
