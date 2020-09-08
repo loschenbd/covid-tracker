29a26d2c3324ef435e4c37328e93ca3762c3cf50
Benjamin Loschen <loschenbd@gmail.com>
2020-09-08 18:27:45 -0500

    Updates

---
+++ precache-manifest.4e10b8c8cf5adc9fa54ab1130d928289.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e90972e774b2b694571a5d560705bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b62d508d295fcbbc003",
+    "url": "/static/css/main.6eb11c34.chunk.css"
+  },
+  {
+    "revision": "5acfe31221285fa304fa",
+    "url": "/static/js/2.d8126d53.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/static/js/2.d8126d53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b62d508d295fcbbc003",
+    "url": "/static/js/main.334b979f.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
